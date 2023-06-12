ef4af1c0930b4f5bd73f80d6909bdb943071e14c
Davi <davimartinsguedes@hotmail.com>
2023-06-12 12:35:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,26 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello2():
+@app.route('/', methods = ['POST','GET'])
+def home():
+    if request.method == 'POST':
+        try:
+            imc = "undefined"
+            if (mass > 1) and (height > 1):
+                divide = true
+            else:
+                divide = false
+            mass = request.form('mass')
+            mass = float(mass)
+            height = request.form('height')
+            height = float(height)
+            if divide:
+                imc = mass/(height*height)
+            return render_template("form.html",imc=imc,mass=mass,height=height,divide=divide)
+        except exception as e:
+            return '<h1>Bad Request : {}</h1>'.format(e)
+    if request.method == 'GET':
+        return render_template("form.html")
     return "Hello"
 
 if __name__ == '__main__':
