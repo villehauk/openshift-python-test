743db6879a71b6d27544769b67efc3fdb182f900
Davi <davimartinsguedes@hotmail.com>
2023-06-13 15:51:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,12 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         try:
-            imc = "undefined"
-            if (mass > 1) and (height > 1):
-                divide = true
-            else:
-                divide = false
             mass = request.form('mass')
             mass = float(mass)
             height = request.form('height')
             height = float(height)
-            if divide:
-                imc = mass/(height*height)
-            return render_template("form.html",imc=imc,mass=mass,height=height,divide=divide)
+            imc = mass/(height*height)
+            return render_template("form.html",imc=imc,mass=mass,height=height)
         except exception as e:
             return '<h1>Bad Request : {}</h1>'.format(e)
     if request.method == 'GET':
